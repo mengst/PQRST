f054ea224576103a2666aa38a3cace81631b75a4
Magnus Engstrom <mengst@kth.se>
2014-05-14 14:40:45 +0200

    La till en mening

--- ecg.cpp
+++ ecg.cpp
@@ -54,7 +54,7 @@ void main(){
     int msec = int(((double)size / sr) * 1000.0);
 	int annNum = 0;
 	annNum = ann.GetEcgAnnotationSize();
-	
+	wprintf(L"Hej hej");
 	wprintf(L"Total beats: %d\n", ann.GetQrsNumber()); 
 	for (int i = 0; i < ann.GetQrsNumber(); i++) {
                                         int smpl = qrsAnn[2*i][0];
